f47cbdc9a5b74f9dd345c32d1a26ddedd842710d
Andrew Furey <andrewfurey2003@gmail.com>
2025-01-31 12:51:51 +0000

    stop reallocating memory when loading mnist (issue #2)

--- examples/mnist.c
+++ examples/mnist.c
@@ -1,4 +1,5 @@
-// model based off of https://github.com/pytorch/examples/blob/main/mnist/main.py
+// model based off of
+// https://github.com/pytorch/examples/blob/main/mnist/main.py
 #include "../include/tensor.h"
 #include "assert.h"
 #include "stdio.h"
@@ -9,6 +10,7 @@
 #include <time.h>
 
 #define MNIST_IMAGE_SIZE 28
+#define MNIST_TEST_SIZE 10000
 
 int randi(int min, int max) {
   int value = roundf((float)rand() / (float)RAND_MAX * (max - min) + min);
@@ -61,37 +63,44 @@ void load_mnist_buffer(const char *image, float *input, float *output,
   }
 }
 
-void load_mnist_batch(tt **input_batch, tt **output_batch,
-                      const char *file_name, int file_length, int batch_size, int line) {
-  ttuple *input_shape = ttuple_build(4, batch_size, 1, 28, 28);
-  ttuple *output_shape = ttuple_build(1, batch_size);
-
-  float *input = (float *)malloc(sizeof(float) * batch_size * 1 * 28 * 28);
-  float *output = (float *)malloc(sizeof(float) * batch_size);
-
-  for (int i = 0; i < batch_size; i++) {
-    // int line = randi(1, file_length);
-    char *image = read_mnist_image(file_name, line);
-    load_mnist_buffer(image, input, output,
-                      i * ttuple_prod(input_shape) / batch_size, i);
-    free(image);
-  }
-
-  *input_batch = tt_from_buffer(input_shape, input, false);
-  *output_batch = tt_from_buffer(output_shape, output, false);
-
-  ttuple_free(input_shape);
-  ttuple_free(output_shape);
-
-  free(input);
-  free(output);
+void load_mnist_image(tt *input_batch, tt *output_batch, const char *file_name,
+                      int file_length, int line) {
+  assert(line < file_length);
+  char *image = read_mnist_image(file_name, line);
+  load_mnist_buffer(image, input_batch->data->buffer,
+                    output_batch->data->buffer, 0, 0);
+  free(image);
 }
 
-void display_mnist_image(tt *image) {
+void load_mnist_batch(tt *input_batch, tt *output_batch, const char *file_name,
+                      int file_length, int batch_size) {
+  for (int i = 0; i < batch_size; i++) {
+    int index = randi(0, file_length);
+    char *image = read_mnist_image(file_name, index);
+    load_mnist_buffer(
+        image, input_batch->data->buffer, output_batch->data->buffer,
+        i * ttuple_prod(input_batch->view->shape) / batch_size, i);
+    free(image);
+  }
+}
+
+void display_mnist_image(tt *image, tt *correct, tt *guesses) {
   ttuple *index = ttuple_zeros(4);
   for (int b = 0; b < image->view->shape->items[0]; b++) {
     index->items[0] = b;
-    printf("Batch item #%d:\n", b+1);
+    float answer = correct->data->buffer[b];
+    if (image->view->shape->items[0] > 1) {
+      printf("Batch item #%d\n", b + 1);
+    } else {
+      printf("Single image\n");
+    }
+    printf("Correct Answer: %f", answer);
+    if (guesses != NULL) {
+      printf(", Guess: %f\n", guesses->data->buffer[b]);
+    } else {
+      printf("\n");
+    }
+
     for (int h = 0; h < image->view->shape->items[2]; h++) {
       index->items[2] = h;
       for (int w = 0; w < image->view->shape->items[3]; w++) {
@@ -105,6 +114,7 @@ void display_mnist_image(tt *image) {
       }
       printf("\n");
     }
+    printf("\n");
   }
   ttuple_free(index);
 }
@@ -211,67 +221,62 @@ tt *sparse_categorical_cross_entropy(tt *input, tt *Y) {
 // takes a vector! (1, n)
 // TODO: double check gradients work
 tt *log_softmax(tt *input) {
-  tt* exp_input = tt_exp(input);
-  tt* sum_exp_input = tt_sum(exp_input, -1);
-  tt* log_sum_exp_input = tt_log(sum_exp_input);
-  tt* expanded = tt_expand(log_sum_exp_input, 0, input->data->size);
-  ttuple* new_shape = ttuple_build(1, input->view->shape->items[1]);
-  tt* reshaped_input = tt_reshape(input, new_shape);
+  tt *exp_input = tt_exp(input);
+  tt *sum_exp_input = tt_sum(exp_input, -1);
+  tt *log_sum_exp_input = tt_log(sum_exp_input);
+  tt *expanded = tt_expand(log_sum_exp_input, 0, input->data->size);
+  ttuple *new_shape = ttuple_build(1, input->view->shape->items[1]);
+  tt *reshaped_input = tt_reshape(input, new_shape);
   ttuple_free(new_shape);
   return tt_sub(reshaped_input, expanded);
 }
 
 typedef struct {
-  tt* conv_layer_1;
-  tt* conv_layer_2;
-  tt* linear_layer_1;
-  tt* linear_layer_2;
+  tt *conv_layer_1;
+  tt *conv_layer_2;
+  tt *linear_layer_1;
+  tt *linear_layer_2;
 } mnist_cnn;
 
 int main(void) {
   srand(time(NULL));
+  int batch_size = 8;
 
-  // model architecture:
-  // conv, relu
-  // conv, relu
-  // maxpool2d, flatten
-  // linear, relu
-  // linear, logsoftmax
+  ttuple *input_batch_shape = ttuple_build(4, batch_size, 1, 28, 28);
+  tt *input_batch = tt_zeros(input_batch_shape, false);
 
-  tt *input_batch = NULL;
-  tt *output_batch = NULL;
+  ttuple *output_batch_shape = ttuple_build(2, batch_size, 1);
+  tt *output_batch = tt_zeros(output_batch_shape, false);
 
-  int batch_size = 1;
-  int example = 123;
-
-  load_mnist_batch(&input_batch, &output_batch, "data/mnist_test.csv", 10000,
-                   batch_size, example);
-  display_mnist_image(input_batch);
+  load_mnist_batch(input_batch, output_batch, "data/mnist_test.csv", 10000,
+                   batch_size);
+  display_mnist_image(input_batch, output_batch, NULL);
 
   mnist_cnn model;
-  model.conv_layer_1   = tt_conv_init(ttuple_build(4, 32, 1, 3, 3), 1, 3, true);
-  model.conv_layer_2   = tt_conv_init(ttuple_build(4, 64, 32, 3, 3), 32, 3, true);
-  model.linear_layer_1 = tt_linear_init(ttuple_build(2, 128, 9216), 9216, true);
-  model.linear_layer_2 = tt_linear_init(ttuple_build(2, 10, 128), 128, true);
+  model.conv_layer_1 = tt_conv_init(ttuple_build(4, 32, 1, 3, 3), 1, 3,
+  true); model.conv_layer_2 = tt_conv_init(ttuple_build(4, 64, 32, 3, 3), 32,
+  3, true); model.linear_layer_1 = tt_linear_init(ttuple_build(2, 128, 9216),
+  9216, true); model.linear_layer_2 = tt_linear_init(ttuple_build(2, 10,
+  128), 128, true);
 
   // Conv (Layer 1)
-  tt* l1 = tt_conv2d(input_batch, model.conv_layer_1);
-  tt* l1_activations = tt_relu(l1);
+  tt *l1 = tt_conv2d(input_batch, model.conv_layer_1);
+  tt *l1_activations = tt_relu(l1);
   // Conv (Layer 2)
-  tt* l2 = tt_conv2d(l1_activations, model.conv_layer_2);
-  tt* l2_activations = tt_relu(l2);
+  tt *l2 = tt_conv2d(l1_activations, model.conv_layer_2);
+  tt *l2_activations = tt_relu(l2);
   // Maxpool + Flatten
-  tt* maxpool = tt_maxpool2d(l2_activations, 2);
-  tt* flattened_maxpool = flatten(maxpool, 1);
-  ttuple* new_shape = ttuple_build(3, batch_size, 9216, 1);
-  tt* l3_input = tt_reshape(flattened_maxpool, new_shape);
+  tt *maxpool = tt_maxpool2d(l2_activations, 2);
+  tt *flattened_maxpool = flatten(maxpool, 1);
+  ttuple *new_shape = ttuple_build(3, batch_size, 9216, 1);
+  tt *l3_input = tt_reshape(flattened_maxpool, new_shape);
   // Linear (Layer 3)
-  tt* l3 = tt_matmul(model.linear_layer_1, l3_input);
-  tt* l3_activations = tt_relu(l3);
+  tt *l3 = tt_matmul(model.linear_layer_1, l3_input);
+  tt *l3_activations = tt_relu(l3);
   // Linear (Layer 3)
-  tt* l4 = tt_matmul(model.linear_layer_2, l3_activations);
-  tt* logits = flatten(l4, 1);
+  tt *l4 = tt_matmul(model.linear_layer_2, l3_activations);
+  tt *logits = flatten(l4, 1);
   // log softmax
-  tt* log_probs = log_softmax(logits);
+  tt *log_probs = log_softmax(logits);
   tt_print(log_probs, true, false);
 }
