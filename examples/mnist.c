e68d1f07fa23934a366040718ffb4c677bd0b5f3
Andrew Furey <andrewfurey2003@gmail.com>
2025-01-31 14:36:59 +0000

    removed old linear_layer function

--- examples/mnist.c
+++ examples/mnist.c
@@ -119,37 +119,6 @@ void display_mnist_image(tt *image, tt *correct, tt *guesses) {
   ttuple_free(index);
 }
 
-// 2d matmul
-tt *linear_layer(tt *input, tt *weights) {
-  int input_width = input->view->shape->items[1];
-  int input_height = input->view->shape->items[0];
-
-  int weights_width = weights->view->shape->items[1];
-  int weights_height = weights->view->shape->items[0];
-
-  assert(input_width == weights_height);
-
-  ttuple *new_input_shape = ttuple_build(3, input_height, input_width, 1);
-  tt *reshaped_input = tt_reshape(input, new_input_shape);
-
-  ttuple *new_weights_shape = ttuple_build(3, 1, weights_height, weights_width);
-  tt *reshaped_weights = tt_reshape(weights, new_weights_shape);
-
-  tt *expanded_input = tt_expand(reshaped_input, 2, weights_width);
-  tt *expanded_weights = tt_expand(reshaped_weights, 0, input_height);
-
-  tt *mul = tt_mul(expanded_input, expanded_weights);
-
-  tt *output = tt_sum(mul, 1);
-
-  ttuple *new_output_shape = ttuple_zeros(2);
-  new_output_shape->items[0] = output->view->shape->items[0];
-  new_output_shape->items[1] = output->view->shape->items[2];
-
-  tt *reshaped_output = tt_reshape(output, new_output_shape);
-  return reshaped_output;
-}
-
 tt *flatten(tt *input, int start_dim) {
   assert(start_dim >= 0 && start_dim < input->view->shape->size);
   ttuple *new_shape = ttuple_zeros(start_dim + 1);
